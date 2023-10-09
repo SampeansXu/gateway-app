e66db2a6765ced34066656678d4a5791fa32f808
SampeansXu <sampeans@163.com>
2023-10-09 23:29:53 +0800

    init

---
+++ src/main/java/cn/huanju/edu100/gateway/app/GatewayAppApplication.java
@@ -0,0 +1,13 @@
+package cn.huanju.edu100.gateway.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayAppApplication.class, args);
+    }
+
+}
